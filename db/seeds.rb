0bde601531c97cdc4dd3e244fb3bbaf40fab0c14
John Travers <johnk.travers@gmail.com>
2019-10-20 13:27:53 -0600

    Add first page of fish family seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,114 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+FishFamily.create([
+  {
+    name: 'Chaetodontidae',
+    english: 'Butterflyfish',
+    spanish: 'Pez Mariposa',
+    image: 'https://www.fishbase.se/images/thumbnails/gif/tn_CHAETOT0.gif'
+  },
+  {
+    name: 'Sphyraenidae',
+    english: 'Barracudas',
+    spanish: 'Barracudas',
+    image: 'https://www.fishbase.se/images/thumbnails/gif/tn_SPHYRAT0.gif'
+  },
+  {
+    name: 'Nomeidae',
+    english: 'Driftfishes',
+    spanish: 'Flotador',
+    image: 'https://www.fishbase.se/images/thumbnails/gif/tn_NOMEIDT0.gif'
+  },
+  {
+    name: 'Pomacanthidae',
+    english: 'Angelfish',
+    spanish: 'Pez Ángel',
+    image: 'https://www.fishbase.se/images/thumbnails/gif/tn_POMACAT0.gif'
+  },
+  {
+    name: 'Elopidae',
+    english: 'Tarpons',
+    spanish: 'Tarpón',
+    image: 'https://www.fishbase.se/images/thumbnails/gif/tn_ELOPIDT0.gif'
+  },
+  {
+    name: 'Gerreidae',
+    english: 'Mojarras',
+    spanish: 'Mojarra',
+    image: 'https://www.fishbase.se/images/thumbnails/gif/tn_GERREIT0.gif'
+  },
+  {
+    name: 'Acanthuridae',
+    english: 'Surgeonfish',
+    spanish: 'Pez Cirujano',
+    image: 'https://www.fishbase.se/images/thumbnails/gif/tn_ACANTHT1.gif'
+  },
+  {
+    name: 'Chanidae',
+    english: 'Milkfishes',
+    spanish: 'Sabalote',
+    image: 'https://www.fishbase.se/images/thumbnails/gif/tn_CHANIDT0.gif'
+  },
+  {
+    name: 'Kuhliidae',
+    english: 'Flagtails',
+    spanish: 'Bandera',
+    image: 'https://www.fishbase.se/images/thumbnails/gif/tn_KUHLIIT0.gif'
+  },
+  {
+    name: 'Carangidae',
+    english: 'Jacks',
+    spanish: 'Jurel',
+    image: 'https://www.fishbase.se/images/thumbnails/gif/tn_CARANGT0.gif'
+  },
+  {
+    name: 'Kyphosidae',
+    english: 'Sea Chubs',
+    spanish: 'Chopa',
+    image: 'https://www.fishbase.se/images/thumbnails/gif/tn_KYPHOST0.gif'
+  },
+  {
+    name: 'Atherinidae',
+    english: 'Silversides',
+    spanish: 'Pejerrey',
+    image: 'https://www.fishbase.se/images/thumbnails/gif/tn_ATHERIT0.gif'
+  },
+  {
+    name: 'Scombridae',
+    english: 'Mackerels & Tunas',
+    spanish: 'Macarela & Atún',
+    image: 'https://www.fishbase.se/images/thumbnails/gif/tn_SCOMBRT0.gif'
+  },
+  {
+    name: 'Embiotocidae',
+    english: 'Surfperches',
+    spanish: 'Percas',
+    image: 'https://www.fishbase.se/images/thumbnails/gif/tn_EMBIOTT0.gif'
+  },
+  {
+    name: 'Hemiramphidae',
+    english: 'Halfbeaks',
+    spanish: 'Pez Aguja',
+    image: 'https://www.fishbase.se/images/thumbnails/gif/tn_HEMIRAT0.gif'
+  },
+  {
+    name: 'Istiophoridae',
+    english: 'Billfishes',
+    spanish: 'Marlines',
+    image: 'https://www.fishbase.se/images/thumbnails/gif/tn_ISTIOPT0.gif'
+  },
+  {
+    name: 'Sparidae',
+    english: 'Porgies',
+    spanish: 'Pargo',
+    image: 'https://www.fishbase.se/images/thumbnails/gif/tn_SPARIDT0.gif'
+  },
+  {
+    name: 'Ephippidae',
+    english: 'Spadefishes',
+    spanish: 'Paguala',
+    image: 'https://www.fishbase.se/images/thumbnails/gif/tn_EPHIPPT0.gif'
+  }
+])
