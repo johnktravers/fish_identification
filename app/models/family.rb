ee8b53e8d461ce76b3daaf20378743ee64b45d1a
John Travers <johnk.travers@gmail.com>
2019-10-31 14:29:16 -0600

    Add validation statements for numericality of family code, genera, and species

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,8 +1,14 @@
 class Family < ApplicationRecord
-  validates_presence_of :name
-  validates_presence_of :english
-  validates_presence_of :spanish
-  validates_presence_of :image
+  validates_presence_of :family_code,
+                        :name,
+                        :english,
+                        :genera,
+                        :species,
+                        :image
+
+  validates_numericality_of :family_code, only_integer: true, greater_than: 0
+  validates_numericality_of :genera, only_integer: true, greater_than: 0
+  validates_numericality_of :species, only_integer: true, greater_than: 0
 
   has_many :fish
 end
